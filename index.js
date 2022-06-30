db3fd2de6fecbe226d882962901c92ca6c25a88c
kaeriv93 <vsan93@yahoo.com>
2022-06-30 18:00:20 -0500

    solved up to 3

---
+++ index.js
@@ -0,0 +1,23 @@
+//1. Write a Javascript function taht reverse a number.
+//Example x = 32243 Output: 34233
+
+//Answer
+
+// const reverseNum= (input) =>{
+//     return input.toString().split("").reverse().join("");
+   
+// }
+// console.log(reverseNum(32243))
+
+//2. Write a Javascript function that checks wheather a passed string is palindrome or not?
+//Answer
+// const isPalinDrome =(str) =>{
+//     let reverseString = str.split("").reverse().join("")
+//     if(reverseString === str){
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+// console.log(isPalinDrome('racecar'))
